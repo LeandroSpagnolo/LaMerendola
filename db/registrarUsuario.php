061f5822f01aa3e96c1acfbac165ec7d2aef3cca
Leandro <leandr0fox561@gmail.com>
2022-05-23 14:04:43 -0300

    Agregado control de error

--- db/registrarUsuario.php
+++ db/registrarUsuario.php
@@ -11,19 +11,18 @@ $emailUsuario = $_POST['email'];
 $conn = new mysqli($HOST,$USER,$PASS,$BASE);
 
 //checkear la conexion
+$conn = new mysqli($HOST,$USER,$PASS,$BASE);
 if($conn->connect_error){
-    die('Connection Failed : '.$conn->connect_error);
+	echo "$conn->connect_error";
+	die("Connection Failed : ". $conn->connect_error);
 }
 else{
-    $stmt = $conn->prepare("insert into credencialesUsuarios(nombre, contraseña, email)
-    values(?, ?, ?)");
-
-    $stmt->bind_param("sss",$nombreUsuario, $contraseñaUsuario, $emailUsuario);
-
-    $stmt->execute();
-    echo "Usuario registrado";
-    
-    $stmt->close();
-    $conn->close();
+	$stmt = $conn->prepare("insert into credencialesUsuarios(nombre, contraseña, email) values(?, ?, ?)");
+	$stmt->bind_param("sss",$nombreUsuario,$contraseñaUsuario,$emailUsuario);
+	$execval = $stmt->execute();
+	echo $execval;
+	echo "Registration successfully...";
+	$stmt->close();
+	$conn->close();
 }
 ?>
